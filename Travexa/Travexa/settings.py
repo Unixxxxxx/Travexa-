76bffb0e91668be6ea6e47e8284315883ed893fd
Sudhanshu Sharma <138148726+Unixxxxxx@users.noreply.github.com>
2025-11-13 00:38:26 +0000

    ***__make_it_ready for the deployment& test the db______^^^

--- Travexa/Travexa/settings.py
+++ Travexa/Travexa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-d8#x4zyj4b&o=paa#2j8)2=5uqma*_xi8@1iu25p*vcf$tqxah
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
